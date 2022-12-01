8ee370ed5e2323cc2d8360664e5580d46c5d2203
PrLangRiko <119633811+PrLangRiko@users.noreply.github.com>
2022-12-02 00:05:52 +0100

    Create string.js

---
+++ Import/string.js
@@ -0,0 +1,93 @@
+function charAt(a,x){
+return a.charAt(x)
+}
+function charCodeAt(a,x){
+    return a.charCodeAt(x)
+}
+function codePointAt(a,x){
+    return a.codePointAt(x)
+}
+function concat(a,x){
+    return a.concat(x)
+}
+function endsWith(a,x,y){
+    return a.endsWith(x,y)
+}
+function includes(a,x){
+    return a.includes(x)
+}
+function indexOf(a,x,y){
+    return a.indexOf(x,y)
+}
+function lastIndexOf(a,x,y){
+    return a.lastIndexOf(x,y)
+}
+function length(a){
+    return a.length()
+}
+function localeCompare(a,x){
+    return a.localeCompare(x)
+}
+function match(a,x){
+    return a.match(x)
+}
+function matchAll(a,x,y){
+    return a.matchAll(x,y)
+}
+function normalize(a){
+    return a.normalize()
+}
+function padEnd(a,x,y){
+    return a.padEnd(x,y)
+}
+function padStart(a,x,y){
+    return a.padStart(x,y)
+}
+function repeat(a,x){
+    return a.repeat(x)
+}
+function replace(a,x,y){
+    return a.replace(x,y)
+}
+function search(a,x){
+    return a.search(x)
+}
+function slice(a,x,y){
+    return a.slice(x,y)
+}
+function split(a,x,y){
+    return a.split(x,y)
+}
+function startsWith(a,x,y){
+    return a.startsWith(x,y)
+}
+function substring(a,x,y){
+    return a.substring(x,y)
+}
+function toLocaleLowerCase(a,x){
+    return a.toLocaleLowerCase(x)
+}
+function toLocaleUpperCase(a,x){
+    return a.toLocaleUpperCase(x)
+}
+function toLowerCase(a){
+    return a.toLowerCase()
+}
+function toString(a){
+    return a.toString()
+}
+function toUpperCase(a){
+    return a.toUpperCase()
+}
+function trim(a){
+    return a.trim()
+}
+function trimEnd(a){
+    return a.trimEnd()
+}
+function trimStart(a){
+    return a.trimStart()
+}
+function valueOf(a){
+    return a.valueOf()
+}
