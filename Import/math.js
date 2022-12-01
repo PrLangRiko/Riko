c952bdcd163074693da88849d920a05c5b56125d
PrLangRiko <119633811+PrLangRiko@users.noreply.github.com>
2022-12-01 23:28:29 +0100

    Add files via upload

---
+++ Import/math.js
@@ -0,0 +1,114 @@
+function E(){
+    return Math.E
+}
+function LN2(){
+    return Math.LN2
+}
+function LN10(){
+    return Math.LN10
+}
+function LOG2E(){
+    return Math.LOG2E
+}
+function LOG10E(){
+    return Math.LOG10E
+}
+function SQRT1_2(){
+    return Math.SQRT1_2
+}
+function abs(x){
+    return Math.abs(x)
+}
+function acos(x){
+    return Math.acos(x)
+}
+function acosh(x){
+    return Math.acosh(x)
+}
+function asin(x){
+    return Math.asin(x)
+}
+function asinh(x){
+    return Math.asinh(x)
+}
+function atan(x){
+    return Math.atan(x)
+}
+function atanh(x){
+    return Math.atanh(x)
+}
+function atan2(y,x){
+    return Math.atan2(y,x)
+}
+function cbrt(x){
+    return Math.cbrt(x)
+}
+function ceil(x){
+    return Math.ceil(x)
+}
+function clz32(x){
+    return Math.clz32(x)
+}
+function cos(x){
+    return Math.cos(x)
+}
+function cosh(x){
+    return Math.cosh(x)
+}
+function exp(x){
+    return Math.exp(x)
+}
+function expm1(x){
+    return Math.expm1(x)
+}
+function floor(x){
+    return Math.floor(x)
+}
+function fround(x){
+    return Math.fround(x)
+}
+function imul(x,y){
+    return Math.imul(x,y)
+}
+function log(x){
+    return Math.log(x)
+}
+function log1p(x){
+    return Math.log1p(x)
+}
+function log10(x){
+    return Math.log10(x)
+}
+function log2(x){
+    return Math.log2(x)
+}
+function pow(x,y){
+    return Math.pow(x,y)
+}
+function JS_random(){
+    return Math.random()
+}
+function round(x){
+    return Math.round(x)
+}
+function sign(x){
+    return Math.sign(x)
+}
+function sin(x){
+    return Math.sin(x)
+}
+function sinh(x){
+    return Math.sinh(x)
+}
+function sqrt(x){
+    return Math.sqrt(x)
+}
+function tan(x){
+    return Math.tan(x)
+}
+function tanh(x){
+    return Math.tanh(x)
+}
+function trunc(x){
+    return Math.trunc(x)
+}
